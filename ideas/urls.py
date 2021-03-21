cd8cda82104978d92f5af8c3e4d9894e295aa5cd
codeamazone <e.niggeme@gmail.com>
2021-03-21 18:11:15 +0100

    added update view and edited templates

--- ideas/urls.py
+++ ideas/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from .views import (
     # HomePageView,
     IdeaListView,
-    IdeaDetailView
-    # ArticleUpdateView,
-    # ArticleDeleteView,
-    # ArticleCreateView,
+    IdeaDetailView,
+    IdeaUpdateView,
+    # IdeaDeleteView,
+    # IdeaCreateView,
 )
 
 urlpatterns = [
+    path('<int:pk>/edit', IdeaUpdateView.as_view(), name='idea_update'),
+    path('<int:pk>', IdeaDetailView.as_view(), name='idea_detail'),
     path('', IdeaListView.as_view(), name='idea_list'),
-    path('<int:pk>', IdeaDetailView.as_view(), name='idea_detail')
 ]
--- ideas/views.py
+++ ideas/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView
 # from django.views.generic.edit import UpdateView, DeleteView, CreateView
 # from django.urls import reverse_lazy
 from .models import Idea
@@ -13,3 +14,9 @@ class IdeaListView(ListView):
 class IdeaDetailView(DetailView):
     model = Idea
     template_name = 'idea_detail.html'
+
+
+class IdeaUpdateView(UpdateView):
+    model = Idea
+    fields = ('idea', 'kind', 'description', 'next_steps', 'status')
+    template_name = 'idea_update.html'
