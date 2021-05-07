ed12d0531c19ba0aa0efea3406ddfed65f0c5e03
codeamazone <e.niggeme@gmail.com>
2021-05-07 15:52:24 +0200

    wrote choices classes for Idea model

--- ideas/models.py
+++ ideas/models.py
@@ -2,39 +2,29 @@ from django.db import models
 from django.urls import reverse
 
 
+class IdeaStatus(models.IntegerChoices):
+    PENDING = 0, 'Pending'
+    ONGOING = 1, 'In progress'
+    COMPLETED = 2, 'Completed'
+
+
+class IdeaKind(models.IntegerChoices):
+    VISION = 0, 'Vision'
+    TRY_SOMETHING = 1, 'Try something'
+    PROJECT = 2, 'Project'
+
+
 class Idea(models.Model):
-    PENDING = 'Pending'
-    ONGOING = 'In Progress'
-    COMPLETED = 'Completed'
-
-    STATUS_CHOICES = [
-        (PENDING, 'Pending'),
-        (ONGOING, 'In progress'),
-        (COMPLETED, 'Completed')
-    ]
-
-    VISION = 'Vision'
-    TRY_SOMETHING = 'Try something'
-    PROJECT = 'Project'
-
-    KIND_CHOICES = [
-        (VISION, 'Vision'),
-        (TRY_SOMETHING, 'Try something'),
-        (PROJECT, 'Project')
-    ]
-
     idea = models.CharField(max_length=200)
-    kind = models.CharField(
-        max_length=15,
-        choices=KIND_CHOICES,
-        default=VISION
+    kind = models.IntegerField(
+        default=IdeaKind.VISION,
+        choices=IdeaKind.choices
     )
     description = models.TextField()
     next_steps = models.TextField(null=True)
-    status = models.CharField(
-        max_length=11,
-        choices=STATUS_CHOICES,
-        default=PENDING
+    status = models.IntegerField(
+        default=IdeaStatus.PENDING,
+        choices=IdeaStatus.choices
     )
 
     def __str__(self):
@@ -44,5 +34,4 @@ class Idea(models.Model):
         return reverse('idea_detail', args=[str(self.id)])
 
 
-# Write tests
-# TODO: create Todos model connected to next_steps from ideas model
+# TODO: Write tests
